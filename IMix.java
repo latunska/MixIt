305757fbe32406ad4c8bc4cc05863c6deb108c5c
CariQ <quiglcar@mail.gvsu.edu>
2015-11-26 20:24:50 -0500

    -a

--- IMix.java
+++ IMix.java
@@ -1,5 +1,10 @@
 package project4;
 
 public interface IMix {
+	/** set the original message into the linked list of characters */
+	void setInitialMessage(String message);
+
+	/** processes the given mix command and returns the current message after processing the mix command */
+	String processCommand(String command);
 
 }
