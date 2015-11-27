a75d13a3af5a142a73bd942c6ebd524c0ac76b5a
CariQ <quiglcar@mail.gvsu.edu>
2015-11-26 22:49:28 -0500

    Added cut and paste

--- Mix.java
+++ Mix.java
@@ -6,8 +6,11 @@ public class Mix implements IMix {
 	/** Linked list of characters representing a message (string) */ 
 	private LinkedList<Character> message;
 	
+	private LinkedList<Character> clipboard;
+	
 	public Mix() {
 		message = new LinkedList<Character>();
+		clipboard = new LinkedList<Character>();
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("Enter intial message: ");
 		String input = scnr.nextLine();
@@ -42,19 +45,31 @@ public class Mix implements IMix {
 			Character adding = tokens[1].charAt(0);
 			message.addToEnd(adding);
 		}
-		if (tokens[0].equalsIgnoreCase("b")) {
+		else if (tokens[0].equalsIgnoreCase("b")) {
 			Character c = tokens[1].charAt(0);
 			int pos = Integer.parseInt(tokens[2]);
 			message.addBeforePosition(pos, c);
 		}
-		if (tokens[0].equalsIgnoreCase("r")) {
+		else if (tokens[0].equalsIgnoreCase("r")) {
 			int pos = Integer.parseInt(tokens[1]);
 			message.removeAtPosition(pos);
 		}
-		if (tokens[0].equalsIgnoreCase("w")) {
+		else if (tokens[0].equalsIgnoreCase("w")) {
 			int first = Integer.parseInt(tokens[1]);
 			int second = Integer.parseInt(tokens[2]);
 			message.switchPositions(first, second);
+		}
+		else if (tokens[0].equalsIgnoreCase("x")) {
+			int start = Integer.parseInt(tokens[1]);
+			int end = Integer.parseInt(tokens[2]);
+			cutToClipboard(start, end);
+		}
+		else if (tokens[0].equalsIgnoreCase("p")) {
+			int start = Integer.parseInt(tokens[1]);
+			pasteFromClipboard(start);
+		}
+		else {
+			System.out.println("Command not found");
 		}
 		//Displays current message with position numbers above
 		String str = "Message: \n\t";
@@ -67,6 +82,32 @@ public class Mix implements IMix {
 		}
 		str += "\n\t" + message.getCurrentMessage();
 		return str;
+	}
+	
+	private void cutToClipboard(int start, int end) {
+		if (start > end || start < 0 || end > message.getCounter()) {
+			throw new IllegalArgumentException();
+		}
+		for (int i = start; i <= end; i++) {
+			Character b = message.removeAtPosition(start);
+			System.out.println(b);
+			clipboard.addToEnd(b);
+			//clipboard.addToEnd(message.removeAtPosition(start));
+		}
+	}
+	
+	//Note: change to addBeforePosition(start + i - 1,
+	//if needs to paste before given position
+	private void pasteFromClipboard(int start) {
+		if (start < 0 || start > message.getCounter()) {
+			throw new IllegalArgumentException();
+		}
+		//Iterates through clipboard and adds to message
+		else {
+			for (int i = 1; i < clipboard.getCounter() + 1; i++) {
+				message.addBeforePosition(start + i, clipboard.copyAtPosition(i));
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
