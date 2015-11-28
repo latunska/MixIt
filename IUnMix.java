ad82a94c8b13c032aaee88d9bbf7563872c099f4
CariQ <quiglcar@mail.gvsu.edu>
2015-11-28 16:47:34 -0500

    Updated for checking if file corrupted

--- IUnMix.java
+++ IUnMix.java
@@ -4,7 +4,8 @@ public interface IUnMix {
 	/** Us the parameter mixedMessage as the string point, that is, 
 	 * the mix message from the previous step.  The second parameter is
 	 * the file where the list of undo commands is found. The returned
-	 * string should be the original message. */
-	String UnMixUsingFile (String filename, String mixedMessage);
+	 * string should be the original message. 
+	 * @throws Exception */
+	String UnMixUsingFile (String filename, String mixedMessage) throws Exception;
 
 }
