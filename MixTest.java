0c1ff3bf763fc1d3b2e3105c2d9b96d19654c537
CariQ <quiglcar@mail.gvsu.edu>
2015-12-02 09:44:00 -0500

    Update MixTest.java

--- MixTest.java
+++ MixTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class MixTest {
 	@Test 
-    public void testProcessCommand() {
+    	public void testProcessCommand() {
         Mix message = new Mix();
         message.setInitialMessage ("This is a secret message");
         String userMessage = message.processCommand("b a 0");
