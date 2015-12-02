e984a4e040ea06714910885dd404e29f4b185f72
CariQ <quiglcar@mail.gvsu.edu>
2015-12-02 01:13:34 -0500

    Update UnMixTest.java

--- UnMixTest.java
+++ UnMixTest.java
@@ -13,7 +13,8 @@ public class UnMixTest {
         	message.processCommand("s testIt");
       
         	UnMix unMessage = new UnMix();
-        	String original = unMessage.UnMixUsingFile ("testIt", userMessage);
+        	String original = unMessage.UnMixUsingFile ("testIt", 
+        			userMessage);
         	assertEquals(original, "This is a secret message");
 	}
 	@Test 
@@ -26,7 +27,8 @@ public class UnMixTest {
         	message.processCommand("s testing.txt");
       
         	UnMix unMessage = new UnMix();
-        	String original = unMessage.UnMixUsingFile ("testing.txt", userMessage);
+        	String original = unMessage.UnMixUsingFile ("testing.txt", 
+        			userMessage);
         	assertEquals(original, "This is a secret message");
 	}
 	@Test 
@@ -39,7 +41,22 @@ public class UnMixTest {
         	message.processCommand("s testing.txt");
       
         	UnMix unMessage = new UnMix();
-        	String original = unMessage.UnMixUsingFile ("testing.txt", userMessage);
+        	String original = unMessage.UnMixUsingFile ("testing.txt", 
+        			userMessage);
         	assertEquals(original, "This is a secret message");
+	}
+	@Test 
+    public void testUndoTestAllWithoutEndErase() {
+        	UnMix unMessage = new UnMix();
+        	String original = unMessage.UnMixUsingFile("testingAll.txt", 
+        			"sting 0t3 all co0t3mmnds.eWsting ");
+        	assertEquals(original, "Testing all commands.");
+	}
+	@Test 
+    public void testUndoTestAll2() {
+        	UnMix unMessage = new UnMix();
+        	String original = unMessage.UnMixUsingFile("testingAll2."
+        			+ "txt", "");
+        	assertEquals(original, "Testing all commands.");
 	}
 }
