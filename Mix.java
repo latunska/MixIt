8edf1c201303b01c398cf2c47a6a86dc0d61ef52
CariQ <quiglcar@mail.gvsu.edu>
2015-12-01 23:28:23 -0500

    Update Mix.java

--- Mix.java
+++ Mix.java
@@ -43,6 +43,10 @@ public class Mix implements IMix {
 	/** Holds a character that has been removed */
 	private Character removed;
 	
+	/*****************************************************************
+	Default constructor for Mix class. Requires calling methods 
+	individually in order to set message and process commands.
+	******************************************************************/
 	public Mix() {
 		message = new LinkedList<Character>();
 		clipboard = new LinkedList<Character>();
@@ -56,9 +60,13 @@ public class Mix implements IMix {
 	}
 	
 	/*****************************************************************
-	Constructor for Mix class.
+	Constructor for Mix class when provided with initial message to
+	begin with. This automatically prompts user for commands.
+	
+	@param input The user's initial message.
 	******************************************************************/
 	public Mix(String input) {
+		//Instantiates the linkedlists for main message and clipboard
 		message = new LinkedList<Character>();
 		clipboard = new LinkedList<Character>();
 		commands = new ArrayList<String>();
@@ -68,9 +76,11 @@ public class Mix implements IMix {
 		isCut = false;
 		//Sets don't write to false
 		dontWrite = false;
-		//Instantiates a scanner and prompts user for inputs
+		//Instantiates a scanner to prompt user for commands
 		Scanner scnr = new Scanner(System.in);
+		//Sets the initial message
 		setInitialMessage(input);
+		//Displays formatted message to user
 		System.out.println(displayCurrentMessage());
 		String command = "";
 		//Loops until user enters 'Q' or 'q'
@@ -78,16 +88,19 @@ public class Mix implements IMix {
 			//Prompts user for command and stores in variable
 			System.out.println("Command: ");
 			command = scnr.nextLine();
+			//Breaks out of loops if command is q
 			if (command.equalsIgnoreCase("Q")) {
 				break;
 			}
+			//Else, processes command and displays formatted message
 			else {
 				processCommand(command);
 				System.out.println(displayCurrentMessage());
 			}
 		}
 		//Prints out final message once user enters 'q'
-		System.out.println("Final Message: " + message.getFinalMessage());
+		System.out.println("Final Message: " + message
+				.getFinalMessage());
 	}
 	
 	/*****************************************************************
@@ -114,98 +127,125 @@ public class Mix implements IMix {
 	******************************************************************/
 	@Override
 	public String processCommand(String command) {
-		isPaste = false;
-		isRemoval = false;
-		isCut = false;
-		dontWrite = false;
+//		isPaste = false;
+//		isRemoval = false;
+//		isCut = false;
+//		dontWrite = false;
 		String currentMessage = message.getFinalMessage();
 		//Splits command into array of Strings
 		String[] tokens = command.split(" ");
 		//Attempts to interpret command
 		try {
+			//Processes append commands
 			if (tokens[0].equalsIgnoreCase("a")) {
 				Character adding;
+				//Determines if appending space
 				if (tokens.length == 1 && command.length() >= 3) {
 					adding = ' ';
 				}
+				//Otherwise appends character normally
 				else {
 					adding = tokens[1].charAt(0);
 				}
 				message.addToEnd(adding);
 			}
+			//Processes insert commands
 			else if (tokens[0].equalsIgnoreCase("b")) {
 				Character c;
 				int pos;
+				//Sets position and character if inserting space
 				if (tokens[1].length() == 0) {
 					c = ' ';
 					pos = Integer.parseInt(tokens[3]);
 				}
+				//Sets position and character otherwise
 				else {
 					c = tokens[1].charAt(0);
 					pos = Integer.parseInt(tokens[2]);
 				}
 				message.addBeforePosition(pos, c);
 			}
+			//Processes remove command
 			else if (tokens[0].equalsIgnoreCase("r")) {
 				int pos = Integer.parseInt(tokens[1]);
+				//Saves removed character for unmixing
 				removed = message.removeAtPosition(pos);
+				//Notifies saveCommand to add removed
 				isRemoval = true;
 			}
+			//Processes switch command
 			else if (tokens[0].equalsIgnoreCase("w")) {
 				int first = Integer.parseInt(tokens[1]);
 				int second = Integer.parseInt(tokens[2]);
 				message.switchPositions(first, second);
 			}
+			//Processes cut command
 			else if (tokens[0].equalsIgnoreCase("x")) {
+				//Notifies saveCommand that command is a cut
 				isCut = true;
 				int start = Integer.parseInt(tokens[1]);
 				int end = Integer.parseInt(tokens[2]);
 				cutToClipboard(start, end);
 			}
+			//Processes paste command
 			else if (tokens[0].equalsIgnoreCase("p")) {
+				//Notifies save command that command is a paste
 				isPaste = true;
 				int start = Integer.parseInt(tokens[1]);
 				pasteFromClipboard(start);
 			}
+			//Processes copy command
 			else if (tokens[0].equalsIgnoreCase("c")) {
 				int start = Integer.parseInt(tokens[1]);
 				int end = Integer.parseInt(tokens[2]);
 				copyToClipboard(start, end);
+				dontWrite = true;
 			}
+			//Processes save command
 			else if (tokens[0].equalsIgnoreCase("s")) {
 				String filename = tokens[1];
+				//Tries to save to file and notifies user if error
 				try {
 					save(filename);
 				} catch (IOException e) {
 					System.out.println("Could not create file.");
 				}
-			}
-			else if (tokens[0].equalsIgnoreCase("h")) {
-				System.out.println(getInstructions());
 				dontWrite = true;
 			}
+			//Processes help command
+			else if (tokens[0].equalsIgnoreCase("h")) {
+				System.out.println(getInstructions());
+				//Notifies saveCommand to ignore this command
+				dontWrite = true;
+			}
+			//Processes any other input
 			else {
 				System.out.println("Command not found");
+				//Notifies saveCommand to ignore this command
 				dontWrite = true;
 			}
 			saveCommand(command);
 		}
 		//Catches NumFormatException and informs reader of error
 		catch (NumberFormatException e) {
-			System.out.println("Command has a character where a number should be.");
+			System.out.println("Command has a character where a number"
+					+ " should be.");
 		}
 		//Catches IllegalArgumentExceptions (which mean that something
 		//was out of range or in the wrong order)
 		catch (IllegalArgumentException e) {
-			System.out.println("Numbers in command out of range or order.");
+			System.out.println("Numbers in command out of range or "
+					+ "order.");
 		}
 		//Catches instances where command is missing a character or two
 		catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Command requires another part.");
 		}
+		//Catches instances where command is missing a part
 		catch (StringIndexOutOfBoundsException e) {
 			System.out.println("Command requires another part.");
 		}
+		//Catches any other possible errors
 		catch (Exception e) {
 			System.out.println("Please check command and try again.");
 		}
@@ -231,7 +271,7 @@ public class Mix implements IMix {
 	
 	private void cutToClipboard(int start, int end) {
 		//Throws exception if arguments out of order or out of bounds
-		if (start > end || start < 0 || end > message.getCounter() - 1) {
+		if (start > end || start < 0 || end > message.getCounter() - 1){
 			throw new IllegalArgumentException();
 		}
 		//Erases previous content of clipboard
@@ -251,14 +291,15 @@ public class Mix implements IMix {
 		else {
 			clipboardLength = clipboard.getCounter();
 			for (int i = 1; i < clipboard.getCounter() + 1; i++) {
-				message.addBeforePosition(start + i - 1, clipboard.copyAtPosition(i));
+				message.addBeforePosition(start + i - 1, clipboard
+						.copyAtPosition(i));
 			}
 		}
 	}
 	
 	private void copyToClipboard(int start, int end) {
 		//Throws exception if arguments are out of bounds or order
-		if (start > end || start < 0 || end > message.getCounter() - 1) {
+		if (start > end || start < 0 || end > message.getCounter() - 1){
 			throw new IllegalArgumentException();
 		}
 		else {
@@ -270,7 +311,12 @@ public class Mix implements IMix {
 			}
 		}
 	}
+	/*****************************************************************
+	Saves list of commands to a file
 	
+	@param name Name to save file to
+	@throws IOException Throws if error occurs while saving
+	******************************************************************/
 	public void save(String name) throws IOException {
 		//Creates a new file
 		File file = new File(name);
@@ -305,19 +351,25 @@ public class Mix implements IMix {
 		}
 	}
 	
-	public static String getInstructions() {
+	private static String getInstructions() {
 		//Returns string that constitutes help menu
-		String str = "Commands (not case sensitive): " + "\n\tQ\t\t Quit";
+		String str = "Commands (not case sensitive): " + "\n\tQ\t\t "
+				+ "Quit";
 		str += "\n\tH \t\t display this menu again";
-		str += "\n\ta c\t\t append character";
+		str += "\n\ta c\t\t append character c";
 		str += "\n\tb c #\t\t insert character c before position #";
 		str += "\n\tr #\t\t remove character at position #";
 		str += "\n\tw & #\t\t switch characters at positions & and #";
-		str += "\n\tx & #\t\t cut message between positions & and # (inclusive)";
-		str += "\n\tp #\t\t paste from clipboard starting before position #";
-		str += "\n\tc & #\t\t cut message between positions & and # (inclusive)";
-		str += "\n\ts filename\t saves file to unmix message in text file of given name";
-		str += "\nTo insert or append a space, type 'space' in place of the character";
+		str += "\n\tx & #\t\t cut message between positions & and # "
+				+ "(inclusive)";
+		str += "\n\tp #\t\t paste from clipboard starting before "
+				+ "position #";
+		str += "\n\tc & #\t\t cut message between positions & and # "
+				+ "(inclusive)";
+		str += "\n\ts filename\t saves file to unmix message in text "
+				+ "file of given name";
+		str += "\nTo insert or append a space, type 'space' in place of"
+				+ " the character";
 		return str;
 	}
 	
@@ -351,8 +403,11 @@ public class Mix implements IMix {
 		isCut = false;
 		dontWrite = false;
 	}
-	
+	/*****************************************************************
+	Main method that begins the process of prompting user for input.
+	******************************************************************/
 	public static void main(String[] args) {
+		//Sets up scanner that allows the user to enter initial message
 		Scanner scnr = new Scanner(System.in);
 		System.out.println(getInstructions());
 		System.out.println("Enter initial message: ");
